d00a883b2b21be280ca42be7b9b242fe13c0bf8e
manukakeg <manukakeg@gmail.com>
2024-12-16 15:21:23 +0530

    first commit

---
+++ theme.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    logo: 'JoyAI Documentation',
+    project: {
+      link: 'https://joyai.app',
+    },
+    footer: {
+      text: 'JoyAI Documentation © 2024',
+    },
+  };
+  
